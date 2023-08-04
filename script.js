60530b32c8e895e924aeac0b93640d6e7200337e
Guilherme Silva <guilherme.vcc.silva@gmail.com>
2023-08-04 19:02:02 +0100

    please

--- script.js
+++ script.js
@@ -21,88 +21,60 @@ handleParallax(); // Call the function initially
 // Get all menu items
 const menuItems = document.querySelectorAll('.menu-item');
 
-// Function to handle mouse enter event
-const handleMouseEnter = (item) => {
-  const paragraph = item.querySelector('p');
-  const header = item.querySelector('h3');
-  paragraph.classList.add('active');
-  header.classList.add('active');
-
-  if (!item.isRotated) {
-    item.style.transform = "rotateY(180deg)"; // Add rotation
-    item.isRotated = true;
-  }
-};
-
-// Function to handle mouse leave event
-const handleMouseLeave = (item) => {
-  const paragraph = item.querySelector('p');
-  const header = item.querySelector('h3');
-  paragraph.classList.remove('active');
-  header.classList.remove('active');
-
-  if (item.isRotated) {
-    item.style.transform = ""; // Reset rotation
-    item.isRotated = false;
-  }
-};
-
-// Function to check if the user is on a mobile device
-const isMobileDevice = () => {
-  return window.innerWidth < 768;
-};
-
-// Function to check if the page is visible
-const isPageVisible = () => {
-  return !document.hidden;
-};
-
-// Function to handle touch start and touch end events for mobile devices
-const handleTouchEvents = (item, touchStart) => {
-  if (isPageVisible()) {
-    if (touchStart) {
-      handleMouseEnter(item);
-    } else {
-      handleMouseLeave(item);
-    }
-  }
-};
-
-// Intersection Observer callback function
-const handleIntersection = (entries, observer) => {
-  entries.forEach(entry => {
-    const item = entry.target;
-    if (entry.isIntersecting) {
-      if (isMobileDevice()) {
-        item.addEventListener('touchstart', () => handleTouchEvents(item, true));
-        item.addEventListener('touchend', () => handleTouchEvents(item, false));
-      } else {
-        item.addEventListener('mouseenter', () => handleMouseEnter(item));
-        item.addEventListener('mouseleave', () => handleMouseLeave(item));
-      }
-    } else {
-      item.removeEventListener('mouseenter', () => handleMouseEnter(item));
-      item.removeEventListener('mouseleave', () => handleMouseLeave(item));
-      item.removeEventListener('touchstart', () => handleTouchEvents(item, true));
-      item.removeEventListener('touchend', () => handleTouchEvents(item, false));
-    }
-  });
-};
-
-// Options for Intersection Observer
-const options = {
-  root: null, // Use the viewport as the root
-  rootMargin: '0px', // No offset from the viewport edge
-  threshold: 0.5, // Trigger when at least 50% of the element is visible
-};
-
-// Create a new Intersection Observer
-const observer = new IntersectionObserver(handleIntersection, options);
-
-// Loop through each menu item and observe it
+// Loop through each menu item
 menuItems.forEach(item => {
-  item.isRotated = false;
-  observer.observe(item);
+  let isRotated = false;
+
+  const handleMouseEnter = () => {
+    const paragraph = item.querySelector('p');
+    const header = item.querySelector('h3');
+    paragraph.classList.add('active');
+    header.classList.add('active');
+
+    if (!isRotated) {
+      item.style.transform = "rotateY(180deg)"; // Add rotation
+      isRotated = true;
+    }
+  };
+
+  const handleMouseLeave = () => {
+    const paragraph = item.querySelector('p');
+    const header = item.querySelector('h3');
+    paragraph.classList.remove('active');
+    header.classList.remove('active');
+
+    if (isRotated) {
+      item.style.transform = ""; // Reset rotation
+      isRotated = false;
+    }
+  };
+
+  // Add event listeners for mouse events
+  item.addEventListener('mouseenter', handleMouseEnter);
+  item.addEventListener('mouseleave', handleMouseLeave);
+  
+ // Check if it's a touch device
+ const isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints > 0;
+
+ if (isTouchDevice) {
+   // Add Intersection Observer for touch devices
+   const observerOptions = {
+     root: null, // Use the viewport as the root
+     threshold: 0.5, // When at least 50% of the target is in view
+   };
+
+   const observer = new IntersectionObserver((entries, observer) => {
+     entries.forEach(entry => {
+       if (entry.isIntersecting) {
+         handleMouseEnter();
+       } else {
+         handleMouseLeave();
+       }
+     });
+   }, observerOptions);
+
+   observer.observe(item);
+ }
 });
 
 // Smooth scrolling for navigation links
